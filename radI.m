db65e71d853e3efa68710c6998465d56f77c562f
Dennis Gardner <dennis.gardner@colorado.edu>
2016-10-03 13:40:00 -0700

    function creation

---
+++ radI.m
@@ -0,0 +1,42 @@
+function [ r, z ] = radI( I )
+%RADI - radially integrate the 2D array I
+%   From the center of the 2D array, I, radially integrate outwards. This
+%   function is capable of handling an I with NaN elements. 
+%
+% Syntax:  [output1,output2] = function_name(input1,input2,input3)
+%
+% Inputs:
+%    input1 - Description
+%    input2 - Description
+%    input3 - Description
+%
+% Outputs:
+%    output1 - Description
+%    output2 - Description
+%
+% Example: 
+%    Line 1 of example
+%    Line 2 of example
+%    Line 3 of example
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: OTHER_FUNCTION_NAME1,  OTHER_FUNCTION_NAME2
+
+% Author: Dennis F Gardner
+% JILA, Univeristy of Colorado, 440 UCB, Boulder, CO 80309
+% email: dennis.gardner@colorado.edu
+% Website 1: http://www.github.com/DennisFGardner 
+% Website 2: http://www.linkedin.com/in/dennisfgardner
+% File Creation: Sept. 14th, 2016
+
+%------------- BEGIN CODE --------------
+
+% Enter your executable matlab commands here
+
+
+%------------- END OF CODE --------------
+end
+
