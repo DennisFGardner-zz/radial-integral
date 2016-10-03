6111d0963044e60e94fa91c318fcb7c3b928fa87
Dennis Gardner <dennis.gardner@colorado.edu>
2016-10-03 13:54:59 -0700

    author credit

--- MAIN.m
+++ MAIN.m
@@ -1,1 +1,11 @@
 %% MAIN
+
+% Author: Dennis F Gardner
+% JILA, Univeristy of Colorado, 440 UCB, Boulder, CO 80309
+% email: dennis.gardner@colorado.edu
+% Website 1: http://www.github.com/DennisFGardner 
+% Website 2: http://www.linkedin.com/in/dennisfgardner
+% File Creation: Oct. 3rd, 2016
+
+
+%% 
